93a0c6b2cb4d252099d6ca3abeaf8f4ec7b2464d
Javier de la Rosa <versae@linhd.uned.es>
2020-09-16 17:35:46 +0200

    Adding language codes and other cosmetic changes (#41)
    * Adding a tmp file to git ignore
    
    * Adding languages and some other cosmetic changes
    
    * Removing blank line
    
    * Fixing an error with the expected output of the averell list command
    
    * Removing unnecessay str

--- tests/test_averell.py
+++ tests/test_averell.py
@@ -57,10 +57,10 @@ def test_export_not_downloaded(caplog):
 @patch('averell.utils.CORPORA_SOURCES', _corpora_sources)
 def test_list():
     expected = "\n".join([
-        '  id  name      size      docs    words  granularity    license',
-        '----  --------  ------  ------  -------  -------------  ---------',
-        '   1  testing   22M       4088   381539  stanza         CC-BY',
-        '   2  testing2  22M       4088   381539  stanza         CC-BY\n',
+        '  id  name      lang    size      docs    words  granularity    license',
+        '----  --------  ------  ------  ------  -------  -------------  ---------',
+        '   1  testing   es      22M       4088   381539  stanza         CC-BY',
+        '   2  testing2  fr      22M       4088   381539  stanza         CC-BY\n',
         ])
     runner = CliRunner()
     result = runner.invoke(list_command, [])
